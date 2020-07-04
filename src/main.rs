d23c63226fbfd92d7e4eec4deae143fafdc9703a
Celeo <mattboulanger@fastmail.com>
2020-07-04 10:51:27 -0700

    Various updates

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 use anyhow::{anyhow, Result};
+use lazy_static::lazy_static;
 use log::{debug, info};
+use regex::Regex;
 use roxmltree::Document;
+use serde::Serialize;
 use std::{
-    env,
+    env, fs,
     io::{BufRead, BufReader},
+    path::Path,
     process::{Child, Command, Stdio},
 };
 
 const PATH_TO: &str = "/media/sf_VirtualShareed/enwiki-20200401-pages-articles-multistream.xml.bz2";
 const TAG_SIZE: usize = 7;
+
+lazy_static! {
+    /// Regex for capturing in-wiki links with optional disregarded rename.
+    static ref LINK_REGEX: Regex =
+        Regex::new(r#"\[\[([a-zA-Z0-9- ]+)[|]?[a-zA-Z0-9- ]*\]\]"#).unwrap();
+}
 
 #[derive(Debug)]
 struct PageParseResult {
@@ -19,6 +29,21 @@ struct PageParseResult {
 impl PageParseResult {
     fn new(page: Option<String>, remainder: String) -> Self {
         Self { page, remainder }
+    }
+}
+
+#[derive(Debug, Serialize)]
+struct PageInfo {
+    title: String,
+    links: Vec<String>,
+}
+
+impl PageInfo {
+    fn new(title: &str, links: Vec<String>) -> Self {
+        Self {
+            title: title.to_owned(),
+            links,
+        }
     }
 }
 
@@ -41,20 +66,32 @@ fn try_get_page(content: String) -> Result<PageParseResult> {
     Ok(PageParseResult::new(Some(page_portion), remainder))
 }
 
-fn parse_page(page: &str) -> Result<String> {
-    let doc = Document::parse(page)?;
-    let title_node = doc
-        .descendants()
-        .find(|n| n.has_tag_name("title"))
-        .ok_or_else(|| anyhow!("Could not find <title> tag"))?;
-    let title = title_node
-        .text()
-        .ok_or_else(|| anyhow!("Could not get text from node"))?;
-    debug!("Got title from page: {}", title);
-    Ok(title.to_owned())
+fn extract_links(content: &str) -> Vec<String> {
+    let mut links: Vec<String> = vec![];
+    for cap in LINK_REGEX.captures_iter(content) {
+        links.push((&cap[1]).to_owned());
+    }
+    links
 }
 
-fn monitor_command(cmd: &mut Child) -> Result<Vec<String>> {
+fn parse_page<'a>(doc: &'a Document) -> Result<(&'a str, &'a str)> {
+    let title = doc
+        .descendants()
+        .find(|n| n.has_tag_name("title"))
+        .ok_or_else(|| anyhow!("Could not find <title> tag"))?
+        .text()
+        .ok_or_else(|| anyhow!("Could not get text from title node"))?;
+    debug!("Got title from page: {}", title);
+    let content = doc
+        .descendants()
+        .find(|n| n.has_tag_name("text"))
+        .ok_or_else(|| anyhow!("Could not find <text> node"))?
+        .text()
+        .ok_or_else(|| anyhow!("Could not get text from text node"))?;
+    Ok((title, content))
+}
+
+fn monitor_command(cmd: &mut Child) -> Result<Vec<PageInfo>> {
     let mut pages = vec![];
     let mut buffer = String::new();
 
@@ -74,8 +111,11 @@ fn monitor_command(cmd: &mut Child) -> Result<Vec<String>> {
 
         match result.page {
             Some(p) => {
-                pages.push(parse_page(&p)?);
-                if pages.len() % 100 == 0 {
+                let doc = Document::parse(&p)?;
+                let (title, content) = parse_page(&doc)?;
+                let links = extract_links(&content);
+                pages.push(PageInfo::new(title, links));
+                if pages.len() % 1000 == 0 {
                     info!("Parsed {} pages", pages.len());
                 }
             }
@@ -97,24 +137,34 @@ fn main() -> Result<()> {
         .stdout(Stdio::piped())
         .spawn()?;
 
-    let titles = monitor_command(&mut cmd)?;
+    let pages = monitor_command(&mut cmd)?;
     let status = cmd.wait()?;
-    println!("Exit status is {}", status.code().unwrap_or(0));
-    println!("{}", titles.join(", "));
+    info!("Exit status is {}", status.code().unwrap_or(0));
+    debug!("Writing output file");
+    fs::write(Path::new("output.json"), serde_json::to_string(&pages)?)?;
+    info!("Wrote data to 'output.json'");
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    use super::try_get_page;
+    use super::{extract_links, try_get_page};
 
     #[test]
     fn test_try_get_pages() {
-        let s = "<page>Foobar</page>b";
+        let s = "<page>Foobar</page>b".to_owned();
         let result = try_get_page(s).unwrap();
 
         assert_eq!("<page>Foobar</page>", result.page.unwrap());
         assert_eq!("b", result.remainder);
+    }
+
+    #[test]
+    fn test_extract_links() {
+        let s = "aaa [a] a| [ [[ [[aa]] a aa] [[aaa|aaaa]]";
+        let links = extract_links(s);
+
+        assert_eq!(links, vec!["aa", "aaa"]);
     }
 }
