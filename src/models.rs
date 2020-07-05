ff102cf777192ebfc708cd0b1528b698d61a0ce0
Celeo <mattboulanger@fastmail.com>
2020-07-05 13:47:33 -0700

    Progress, but doesn't most of the time

--- src/models.rs
+++ src/models.rs
@@ -1,28 +1,7 @@
 use serde::Serialize;
 
-#[derive(Debug)]
-pub(crate) struct PageParseResult {
-    pub(crate) page: Option<String>,
-    pub(crate) remainder: String,
-}
-
-impl PageParseResult {
-    pub(crate) fn new(page: Option<String>, remainder: String) -> Self {
-        Self { page, remainder }
-    }
-}
-
 #[derive(Debug, Serialize)]
 pub(crate) struct PageInfo {
     pub(crate) title: String,
     pub(crate) links: Vec<String>,
-}
-
-impl PageInfo {
-    pub(crate) fn new(title: &str, links: Vec<String>) -> Self {
-        Self {
-            title: title.to_owned(),
-            links,
-        }
-    }
 }
--- src/parsing.rs
+++ src/parsing.rs
@@ -1,9 +1,8 @@
 use crate::models::PageInfo;
 use anyhow::{anyhow, Result};
 use lazy_static::lazy_static;
-use log::{debug, info};
+use log::{debug, error, info};
 use regex::Regex;
-use roxmltree::Document;
 use std::{
     io::{BufRead, BufReader},
     process::Child,
@@ -11,6 +10,8 @@ use std::{
 
 lazy_static! {
     /// Regex for capturing in-wiki links with optional disregarded rename.
+    static ref TITLE_REGEX: Regex = Regex::new(r#"<title>([a-zA-Z0-9-/>> ]+)</title>"#).unwrap();
+    static ref CONTENT_REGEX: Regex = Regex::new(r#""#).unwrap();
     static ref LINK_REGEX: Regex =
         Regex::new(r#"\[\[([a-zA-Z0-9- ]+)[|]?[a-zA-Z0-9- ]*\]\]"#).unwrap();
 }
@@ -23,20 +24,26 @@ fn extract_links(content: &str) -> Vec<String> {
     links
 }
 
-fn parse_page<'a>(doc: &'a Document) -> Result<(&'a str, &'a str)> {
-    let title = doc
-        .descendants()
-        .find(|n| n.has_tag_name("title"))
-        .ok_or_else(|| anyhow!("Could not find <title> tag"))?
-        .text()
-        .ok_or_else(|| anyhow!("Could not get text from title node"))?;
-    let content = doc
-        .descendants()
-        .find(|n| n.has_tag_name("text"))
-        .ok_or_else(|| anyhow!("Could not find <text> node"))?
-        .text()
-        .ok_or_else(|| anyhow!("Could not get text from text node"))?;
-    Ok((title, content))
+fn parse_page(content: &str) -> Result<(String, String)> {
+    let title = &TITLE_REGEX
+        .captures(content)
+        .ok_or_else(|| anyhow!("Could not find <title> tag"))?[1];
+
+    let text_start = content
+        .find("<text")
+        .ok_or_else(|| anyhow!("Could not find opening text tag"))?;
+    let text_end = content
+        .find("</text>")
+        .ok_or_else(|| anyhow!("Could not find closing text tag"))?;
+    let text: String = content
+        .chars()
+        .skip(text_start)
+        .skip_while(|&c| c != '>')
+        .skip(2)
+        .take(text_end - text_start)
+        .collect();
+
+    Ok((title.to_owned(), text))
 }
 
 pub(crate) fn watch_command(cmd: &mut Child) -> Result<Vec<PageInfo>> {
@@ -62,11 +69,17 @@ pub(crate) fn watch_command(cmd: &mut Child) -> Result<Vec<PageInfo>> {
             continue;
         }
         if line.ends_with("</page>") {
-            let doc = Document::parse(&buffer)?;
-            let (title, content) = parse_page(&doc)?;
-            let links = extract_links(content);
-            pages.push(PageInfo::new(title, links));
+            let (title, content) = match parse_page(&buffer) {
+                Ok((t, c)) => (t, c),
+                Err(e) => {
+                    error!("Could not parse page: {}", e);
+                    buffer.clear();
+                    continue;
+                }
+            };
+            let links = extract_links(&content);
             debug!("Parsed {}", title);
+            pages.push(PageInfo { title, links });
             if pages.len() % 1000 == 0 {
                 info!("Parsed {} pages", pages.len());
             }
@@ -79,7 +92,7 @@ pub(crate) fn watch_command(cmd: &mut Child) -> Result<Vec<PageInfo>> {
 
 #[cfg(test)]
 mod tests {
-    use super::extract_links;
+    use super::{extract_links, parse_page};
 
     #[test]
     fn test_extract_links() {
@@ -87,5 +100,39 @@ mod tests {
         let links = extract_links(s);
 
         assert_eq!(links, vec!["aa", "aaa"]);
+    }
+
+    #[test]
+    fn test_parse_page2() {
+        let s = r#"
+        <page>
+            <title>AccessibleComputing</title>
+            <ns>0</ns>
+            <id>10</id>
+            <redirect title="Computer accessibility" />
+            <revision>
+            <id>854851586</id>
+            <parentid>834079434</parentid>
+            <timestamp>2018-08-14T06:47:24Z</timestamp>
+            <contributor>
+                <username>Godsy</username>
+                <id>23257138</id>
+            </contributor>
+            <comment>remove from category for seeking instructions on rcats</comment>
+            <model>wikitext</model>
+            <format>text/x-wiki</format>
+            <text bytes="94" xml:space="preserve">#REDIRECT [[Computer accessibility]]
+
+        {{R from move}}
+        {{R from CamelCase}}
+        {{R unprintworthy}}</text>
+            <sha1>42l0cvblwtb4nnupxm6wo000d27t6kf</sha1>
+            </revision>
+        </page>
+        "#;
+        let (title, text) = parse_page(s).unwrap();
+
+        assert_eq!(title, "AccessibleComputing");
+        assert_eq!(text.len(), 156);
     }
 }
